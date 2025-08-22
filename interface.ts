831a27b5ec41cd84772173db57066428ad106f56
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-08-22 13:54:11 +0200

    desctruct + filter

--- interface.ts
+++ interface.ts
@@ -38,3 +38,27 @@ let employee:Employees = {
 export interface Login{
     login():User;
 }
+
+
+let users: User[] = [
+    {name:"John", id:1, email:"john@gmail.com"},
+    {name:"Jill", id:2, email:"jill@gmail.com"},
+    {name:"Jane", id:3, email:"jane@gmail.com"}
+];
+
+//redestructuring
+let [user1, user2, ...restUsers]: User[] = [
+    {name:"John", id:1, email:"john@gmail.com"},
+    {name:"Jill", id:2, email:"jill@gmail.com"},
+    {name:"Jane", id:3, email:"jane@gmail.com"},
+    {name:"Jack", id:4, email:"jack@gmail.com"},
+    {name:"Jenny", id:5, email:"jenny@gmail.com"}
+];
+console.log(user1);
+console.log(user2);
+console.log(restUsers);
+
+
+
+let result = restUsers.filter(user => user.id > 3);
+console.log(result);
