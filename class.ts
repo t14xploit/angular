c96ed38e0c7a77c0c7fdb7d48b046013f9da67ed
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-08-22 13:19:32 +0200

    using constructor

--- class.ts
+++ class.ts
@@ -1,17 +1,22 @@
 class Employee{
-   id!:number; 
+   id:number; 
 
-   name!:string;
+   name:string;
 
-   address!:string;
+   address:string;
 
- 
+   //multiple contructors not allowed in typescript
+   constructor(id:number, name:string, address:string){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
 }
 
-let john = new Employee();
+let john = new Employee(1, "John", "Highway 71");
 
-john.id = 1;
-john.name = "John";
-john.address = "Highway 71";
+// john.id = 1;
+// john.name = "John";
+// john.address = "Highway 71";
 
 console.log(john);
